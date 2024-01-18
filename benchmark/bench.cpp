a00a52022bcd85861df3bc7e4452d42ad57f9d94
Josh G <154947668+mispriced@users.noreply.github.com>
2024-01-18 17:52:29 +0900

    Adding nanobench as well (#2)
    * Adding nanobench as well
    
    * Add optional to common.hpp

--- benchmark/bench.cpp
+++ benchmark/bench.cpp
@@ -1,76 +1,21 @@
 #include <algorithm>  // for std::shuffle
 #include <optional>
-#include <random>  // for std::mt19937 and std::random_device
 
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_test_macros.hpp>
-#include <expected64/expected64.hpp>
 
-enum class error_code
-{
-  no_error = 0,
-  error
-};
+#include "common.hpp"
 
-std::vector<int> gen_shuffled_numbers()
-{
-  std::vector<int> numbers;
-  for (int i = -32; i <= 32; ++i) {
-    numbers.push_back(i);
-  }
-
-  std::random_device rd;
-  std::mt19937       g(rd());
-  std::shuffle(numbers.begin(), numbers.end(), g);
-
-  return numbers;
-}
-
-expected64<int64_t, error_code> factorial_expected64(int n)
-{
-  if (n < 0) {
-    return expected64<int64_t, error_code>(error_code::error);
-  }
-  int64_t result = 1;
-  for (int i = 1; i <= n; ++i) {
-    result *= i;
-  }
-  return expected64<int64_t, error_code>(result);
-}
-
-std::optional<int64_t> factorial_optional(int n)
-{
-  if (n < 0) {
-    return {};
-  }
-  int64_t result = 1;
-  for (int i = 1; i <= n; ++i) {
-    result *= i;
-  }
-  return result;
-}
-
-int64_t factorial_raw(int n)
-{
-  if (n < 0) {
-    return -1;
-  }
-  int64_t result = 1;
-  for (int i = 1; i <= n; ++i) {
-    result *= i;
-  }
-  return result;
-}
-
-TEST_CASE("factorial")
+template<typename T>
+void run_factorial_benchmarks()
 {
   std::vector<int> numbers = gen_shuffled_numbers();
 
   BENCHMARK("Factorial with expected64")
   {
-    int64_t score = 0;
+    T score = 0;
     for (int num : numbers) {
-      auto result = factorial_expected64(num);
+      auto result = factorial_expected64<T>(num);
       score += result.has_error() ? 0 : result.get_value();
     }
     return score;
@@ -78,9 +23,9 @@ TEST_CASE("factorial")
 
   BENCHMARK("Factorial with std::optional")
   {
-    int64_t score = 0;
+    T score = 0;
     for (int num : numbers) {
-      auto result = factorial_optional(num);
+      auto result = factorial_optional<T>(num);
       score += result.has_value() ? result.value() : 0;
     }
     return score;
@@ -88,48 +33,40 @@ TEST_CASE("factorial")
 
   BENCHMARK("Factorial with Raw Type")
   {
-    int64_t score = 0;
+    T score = 0;
     for (int num : numbers) {
-      auto result = factorial_raw(num);
+      auto result = factorial<T>(num);
       score += (result > 0) ? result : 0;
     }
     return score;
   };
 }
 
-auto cube_expected64(double value)
+TEST_CASE("factorial - int64_t")
 {
-  if (value < 0) {
-    return expected64<double, error_code>(error_code::error);
-  }
-  return expected64<double, error_code>(value * value * value);
+  run_factorial_benchmarks<int64_t>();
 }
 
-std::optional<double> cube_optional(double value)
+TEST_CASE("factorial - uint64_t")
 {
-  if (value < 0) {
-    return {};
-  }
-  return {value * value * value};
+  run_factorial_benchmarks<uint64_t>();
 }
 
-auto cube_raw(double value)
+TEST_CASE("factorial - double")
 {
-  if (value < 0) {
-    return -1.0;
-  }
-  return value * value * value;
+  run_factorial_benchmarks<double>();
 }
 
-TEST_CASE("cube")
+template<typename T>
+void run_cube_benchmarks()
 {
   std::vector<int> numbers = gen_shuffled_numbers();
 
   BENCHMARK("Cube with expected64")
   {
-    int64_t score = 0;
+    T score = 0;
     for (int num : numbers) {
-      auto result = cube_expected64(num);
+      auto result = cube_expected64<T>(static_cast<T>(num));
       score += result.has_error() ? 0 : result.get_value();
     }
     return score;
@@ -137,9 +74,9 @@ TEST_CASE("cube")
 
   BENCHMARK("Cube with std::optional")
   {
-    int64_t score = 0;
+    T score = 0;
     for (int num : numbers) {
-      auto result = cube_optional(num);
+      auto result = cube_optional<T>(static_cast<T>(num));
       score += result.has_value() ? result.value() : 0;
     }
     return score;
@@ -147,11 +84,26 @@ TEST_CASE("cube")
 
   BENCHMARK("Cube with Raw Type")
   {
-    int64_t score = 0;
+    T score = 0;
     for (int num : numbers) {
-      auto result = cube_raw(num);
-      score += (result > 0) ? result : 0;
+      auto result = cube<T>(static_cast<T>(num));
+      score += result;
     }
     return score;
   };
+}
+
+TEST_CASE("cube - int64_t")
+{
+  run_cube_benchmarks<int64_t>();
+}
+
+TEST_CASE("cube - uint64_t")
+{
+  run_cube_benchmarks<uint64_t>();
+}
+
+TEST_CASE("cube - double")
+{
+  run_cube_benchmarks<double>();
 }
---
+++ benchmark/common.hpp
@@ -0,0 +1,88 @@
+#pragma once
+#include <optional>
+#include <random>  // for std::mt19937 and std::random_device
+#include <vector>
+
+#include <expected64/expected64.hpp>
+
+std::vector<int> gen_shuffled_numbers()
+{
+  std::vector<int> numbers;
+  for (int i = -10; i < 10; ++i) {
+    for (int j = 0; j < 10; ++j) {
+      numbers.push_back(i);
+    }
+  }
+
+  std::random_device rd;
+  std::mt19937       g(rd());
+  std::shuffle(numbers.begin(), numbers.end(), g);
+
+  return numbers;
+}
+
+enum class error_code
+{
+  no_error = 0,
+  error
+};
+
+template<typename T>
+T factorial(T n)
+{
+  if (n < 0 || n > 50)
+    return static_cast<T>(-1);  // Error case for signed types
+  T result = 1;
+  for (T i = 1; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
+}
+
+template<typename T>
+T factorial_raw(T n)
+{
+  if (n < 0)
+    return static_cast<T>(-1);  // Error case for signed types
+  return factorial(n);
+}
+
+template<typename T>
+std::optional<T> factorial_optional(T n)
+{
+  if (n < 0)
+    return std::nullopt;
+  return factorial(n);
+}
+
+template<typename T>
+expected64<T, error_code> factorial_expected64(T n)
+{
+  if (n < 0)
+    return expected64<T, error_code>(error_code::error);
+  return expected64<T, error_code>(factorial(n));
+}
+
+template<typename T>
+T cube(const T& value)
+{
+  if (value < 0)
+    return static_cast<T>(0);  // Error case for signed types
+  return value * value * value;
+}
+
+template<typename T>
+std::optional<T> cube_optional(const T& value)
+{
+  if (value < 0)
+    return std::nullopt;
+  return cube(value);
+}
+
+template<typename T>
+expected64<T, error_code> cube_expected64(const T& value)
+{
+  if (value < 0)
+    return expected64<T, error_code>(error_code::error);
+  return expected64<T, error_code>(cube(value));
+}
